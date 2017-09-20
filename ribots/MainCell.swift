66b6a4b8baa7e8367ba9b747fd5a5fdc7de15d83
Jano Amigo <janoanieto@gmail.com>
2017-09-20 20:56:44 +0100

    Added MainViewController and MainCell

---
+++ ribots/MainCell.swift
@@ -0,0 +1,56 @@
+//
+//  MainCell.swift
+//  ribots
+//
+//  Created by Jano Amigo on 20/9/17.
+//  Copyright © 2017 Jano Amigo. All rights reserved.
+//
+
+import UIKit
+import FoldingCell
+
+//class MainCell: FoldingCell {
+//
+//    override func animationDuration(_ itemIndex: NSInteger, type: FoldingCell.AnimationType) -> TimeInterval {
+//        let durations = [0.26, 0.2, 0.2]
+//        return durations[itemIndex]
+//    }
+//}
+
+class MainCell: UICollectionViewCell {
+    
+    static let reuseIdentifier: String = "Cell"
+    
+    lazy var top: NSLayoutConstraint = self.background.topAnchor.constraint(equalTo: self.contentView.topAnchor)
+    lazy var left: NSLayoutConstraint = self.background.leftAnchor.constraint(equalTo: self.contentView.leftAnchor)
+    lazy var bottom: NSLayoutConstraint = self.background.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor)
+    lazy var right: NSLayoutConstraint = self.background.rightAnchor.constraint(equalTo: self.contentView.rightAnchor)
+    
+    func setCell() {
+        background.backgroundColor = UIColor.red
+    }
+    
+    lazy var background: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 300 / 2
+        view.clipsToBounds = true
+        
+        return view
+    }()
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.backgroundColor = nil
+        contentView.addSubview(background)
+        
+        NSLayoutConstraint.activate([top, left, bottom, right])
+        
+        
+    }
+}
---
+++ ribots/MainViewController.swift
@@ -0,0 +1,95 @@
+//
+//  MainCollectionViewController.swift
+//  ribots
+//
+//  Created by Jano Amigo on 20/9/17.
+//  Copyright © 2017 Jano Amigo. All rights reserved.
+//
+
+import UIKit
+import BouncyLayout
+
+private let reuseIdentifier = "Cell"
+
+class MainViewController: UIViewController {
+    let cellSize = UIScreen.main.bounds.width*0.75
+    
+    let numberOfItems = 100
+    var randomFloat: CGFloat { return max(45, CGFloat(arc4random_uniform(100))) }
+    
+    lazy var topOffset: [CGFloat] = { (0..<self.numberOfItems).map { _ in CGFloat(arc4random_uniform(250)) } }()
+    
+    var insets: UIEdgeInsets = UIEdgeInsets(top: 200, left: 0, bottom: 200, right: 0)
+    
+    var additionalInsets: UIEdgeInsets = UIEdgeInsets(top: 5, left: 0, bottom: 5, right: 0)
+    
+    lazy var layout = BouncyLayout(style: .regular)
+    
+    lazy var collectionView: UICollectionView = {
+        
+        let view = UICollectionView(frame: .zero, collectionViewLayout: self.layout)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.delaysContentTouches = false
+        view.alwaysBounceVertical = true
+        view.alwaysBounceHorizontal = false
+        view.backgroundColor = nil
+        view.isOpaque = false
+        view.delegate = self
+        view.dataSource = self
+        view.register(MainCell.self, forCellWithReuseIdentifier: MainCell.reuseIdentifier)
+        
+        return view
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        view.clipsToBounds = true
+        
+        collectionView.contentInset = UIEdgeInsets(top: insets.top + additionalInsets.top, left: insets.left + additionalInsets.left, bottom: insets.bottom + additionalInsets.bottom, right: insets.right + additionalInsets.right)
+        collectionView.scrollIndicatorInsets = UIEdgeInsets(top: insets.top, left: insets.left, bottom: insets.bottom, right: insets.right)
+        view.addSubview(collectionView)
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: -insets.top),
+            collectionView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: -insets.left),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: insets.bottom),
+            collectionView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: insets.right)
+            ])
+        
+
+    }
+}
+
+extension MainViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return numberOfItems
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: MainCell.reuseIdentifier, for: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        guard let cell = cell as? MainCell else { return }
+        cell.setCell()
+    }
+}
+
+extension MainViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 300, height: 300)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 15
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 15
+    }
+    
+}
